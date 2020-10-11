67c7c9405b60a2740d42f2dd1c43770c0bf9474d
marioalbornoz <mario.albornozc@utem.cl>
2020-10-11 20:03:26 -0300

    se cambia vista general de carreras

--- src/pages/ContentHome.jsx
+++ src/pages/ContentHome.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import './content.css'
+import FotoCarreras from "../groups.png"
 
 export const ContenrHome = () => {
     return (
       <div id="content" className="p-4 p-md-5 col-lg-10 col-md-9">
-        
-        <h2 className="mb-4" >Folio Estudiantil #07</h2>
-         <h6>Carreras</h6>
-         <li>Ing informatica</li>
+        <h6 className="mb-4 lead">Folio Estudiantil #07</h6>
+        <div className="card shadow col-lg-5 col-md-6 col-sm-12 card-carrera redondeado" >
+          <img className="card-img-top" src={FotoCarreras} alt="" />
+          <div className="card-body codigocarrera">
+            <h2 className="card-title">21030</h2>
+            <p className="card-text">
+              Escuela de ingenieria informatica
+            </p>
+            <a href="/alumnos" class="btn btn-primary">
+              ir a alumnos
+            </a>
+          </div>
+        </div>
       </div>
     );
 }
--- src/pages/Profesores.jsx
+++ src/pages/Profesores.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from 'react'
+import React, {  useContext } from 'react'
 import { UserContext } from '../context/UserContext'
 import "./style.css";
 import imagen from "../perfil-clase.png";
