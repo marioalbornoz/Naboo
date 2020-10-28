d8009f23601236b0d250ce59a16a5a11636e0e10
marioalbornoz <mario.albornozc@utem.cl>
2020-10-28 00:52:44 -0300

    se agrega grafico de barras y pie

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
-import { FolioContext } from '../context/FolioContext'
+import { FolioContext } from '../context/FolioContext';
+import {Pie, Bar} from 'react-chartjs-2'
 
 export const Report = () => {
     const {
@@ -7,9 +8,53 @@ export const Report = () => {
       foliospriorityone,
       foliosprioritytwo,
     //   foliosmes,
-    //   mes,
-    //   guardarMes,
+      // mes,
+      // guardarMes,
     } = useContext(FolioContext);
+
+    const data = {
+      labels:['Prioridad 1', 'Prioridad 2'],
+      datasets:[{
+        data:[(foliospriorityone.length*100/foliostotales), (foliosprioritytwo.length*100/foliostotales)],
+        backgroundColor:['#FF5734', '#73B775'],
+
+      }]
+    };
+    const opciones = {
+      responsive: true,
+    }
+
+    const datosBarra = {
+      labels: [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre",
+      ],
+      datasets: [
+        {
+          label: "Folios",
+          backgroundColor: "#FF5734",
+          borderColor: "black",
+          borderWidth: 1,
+          hoverBackgroundColor: "rgba(31, 111, 208, 1)",
+          hoverBorderColor: "white",
+          data: [1, 2, 3, 4, 5, 6, 7, 8, 9, foliostotales, 11, 12],
+        },
+      ],
+    };
+    const optionBar={
+      maintainAspectRatio:false,
+      responsive:true
+    }
     
     return (
       <div className="col-lg-9 col-md-8 mt-5 perfil">
@@ -17,7 +62,8 @@ export const Report = () => {
           <div className="col">
             <h6 className="lead">Reportes</h6>
             {foliostotales && foliospriorityone && foliosprioritytwo ? (
-              <div>
+              <>
+              <div className="card">
                 <p className="">
                   Cantidad de reportes ingresados: {foliostotales}
                 </p>
@@ -29,7 +75,14 @@ export const Report = () => {
                   Cantidad de reportes ingresados con prioridad 2:{" "}
                   {foliosprioritytwo.length}
                 </p>
+                <Pie data={data} options={opciones}/>
+                
               </div>
+              <div className="card" style={{width:'100% ',height:'500px'}}>
+              <Bar data={datosBarra} options={optionBar} />
+              </div>
+
+              </>
             ) : null}
             {
 
