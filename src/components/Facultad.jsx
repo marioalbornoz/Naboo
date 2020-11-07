b338e1c4f29acb1bafc29b398c7ba1a03e61f165
marioalbornoz <mario.albornozc@utem.cl>
2020-11-06 22:52:57 -0300

    se agregan datos para condicion de visibilidad

--- src/components/Facultad.jsx
+++ src/components/Facultad.jsx
@@ -1,37 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
-import FotoCarreras from "../groups.png"
+import { PerfilContext } from '../context/PerfilContext'
+import { carreraUsuario } from '../helpers'
+import { CarreraCard } from './CarreraCard'
 // import { Spinner } from './Spinner'
 
 export const Facultad = ({carreras, nombrecarrera}) => {
     
-    const [carerasarray, guardarCarreras] = useState([])
+  const perfil = useContext(PerfilContext);
+  const { carrera, escuela, facultad } = perfil.perfil;
+  const [carerasarray, guardarCarreras] = useState([]);
 
     // const carrerasByFacultad  = carreras.filter(carreraFilter=> carreraFilter.facultad === facultad)
     
-    useEffect(()=>{
-        try {
-            const carrerasByFacultades = (nombre) => {
-                console.log(nombrecarrera);
-                const carrerasFacultad = carreras.filter(
-                  (carreraFilter) => carreraFilter.facultad === nombre
-                );
-                guardarCarreras(carrerasFacultad);
-              };
-            carrerasByFacultades(nombrecarrera);
-        } catch (error) {
-            console.error(error);
-        }
-    }, [nombrecarrera, carreras])
-
-
-    const obtenerCarreraCodigo = (id) => {
-      if(id){
-        localStorage.setItem('carrera',id)
+    useEffect(() => {
+      try {
+        const carrerasByFacultades = (nombre) => {
+          console.log(nombrecarrera);
+          const carrerasFacultad = carreras.filter(
+            (carreraFilter) => carreraFilter.facultad === nombre
+          );
+          guardarCarreras(carrerasFacultad);
+        };
+        carrerasByFacultades(nombrecarrera);
+      } catch (error) {
+        console.error(error);
       }
-      return;
-    }
+    }, [nombrecarrera, carreras]);
+
+
+
     return (
       <div className="row">
         <div className="col">
@@ -43,33 +42,18 @@ export const Facultad = ({carreras, nombrecarrera}) => {
           <div className="col">
             {" "}
             <div className="row">
-              {carerasarray ? (
-                carerasarray.map((carrera) => (
-                  <div
-                    key={carrera.id}
-                    className="card shadow col-lg-5 col-md-6 col-sm-12 card-carrera redondeado m-2"
-                  >
-                    <img className="card-img-top" src={FotoCarreras} alt="" />
-                    <div className="card-body codigocarrera">
-                      <h2 className="card-title">{carrera.codigo}</h2>
-                      <p className="card-text">Escuela {carrera.nombre}</p>
-                      <a
-                        href={`/alumnos`}
-                        onClick={
-                          () => {
-                            obtenerCarreraCodigo(carrera.codigo)
-                          }
-                        }
-                        className="btn btn-primary"
-                      >
-                        ir a alumnos
-                      </a>
-                    </div>
-                  </div>
-                ))
-              ) : (
-                <p>No hay carreas ingresadas aun</p>
-              )}
+              {carerasarray && carrera
+                ? carerasarray
+                    .filter(
+                      (carrerafilter) =>
+                        carrerafilter.nombre === carreraUsuario(carrera)
+                    )
+                    .map((carrera) => (
+                      <CarreraCard carrera={carrera} key={carrera.id} />
+                    ))
+                : carerasarray.map((carrera) => (
+                    <CarreraCard carrera={carrera} key={carrera.id} />
+                  ))}
             </div>
           </div>
         </div>
